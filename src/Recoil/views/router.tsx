5ae853e408577a6cadedef0c241e49cbf8951a37
aaely <aaron.ely@hotmail.com>
2021-08-09 12:41:41 -0700

    ...

--- src/Recoil/views/router.tsx
+++ src/Recoil/views/router.tsx
@@ -2,8 +2,8 @@ import { atom, atomFamily, DefaultValue, selector, selectorFamily, SetRecoilStat
 import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist({
-    key: 'recoil-persist', // this key is using to store data in local storage
-    storage: localStorage, // configurate which stroage will be used to store the data
+    key: 'recoil-persist-session', // this key is using to store data in local storage
+    storage: sessionStorage, // configurate which stroage will be used to store the data
   })
 
 export const currentView = atom<string>({
